509878d690b1c3c09a8a63ac0d72488dd04b8a00
Akshat Sharma <webdevakshat@gmail.com>
2024-06-27 10:50:34 +0530

    Navbar Icon Change - Fixed

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
 
 const navToggler = document.querySelector(".nav-toggler");
 const navLinks = document.querySelector(".nav-links");
-const isHamburgerIcon = true;
+let isHamburgerIcon = true;
 const toggle = () => {
-  navLinks.classList.toggle("nav-link-active" , isHamburgerIcon);
- currentNavIcon.src = isHamburgerIcon ? "assets/shared/icon-close.svg" : "assets/shared/icon-hamburger.svg";
+  navLinks.classList.toggle("nav-link-active");
+  navToggler.src = isHamburgerIcon ? "assets/shared/icon-close.svg" : "assets/shared/icon-hamburger.svg";
+  isHamburgerIcon=!isHamburgerIcon
 };
 
 navToggler.addEventListener("click", toggle);
