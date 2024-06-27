e84ab87c06c7a59c1d65d73396fbd5f655d37325
Akshat Sharma <webdevakshat@gmail.com>
2024-06-27 10:30:32 +0530

    Refracted - Navbar Js

--- js/index.js
+++ js/index.js
@@ -2,14 +2,10 @@
 
 const navToggler = document.querySelector(".nav-toggler");
 const navLinks = document.querySelector(".nav-links");
-
+const isHamburgerIcon = true;
 const toggle = () => {
-  navLinks.classList.toggle("nav-link-active");
-  if (navToggler.src.includes("assets/shared/icon-hamburger.svg")) {
-    navToggler.src = "assets/shared/icon-close.svg";
-  } else {
-    navToggler.src = "assets/shared/icon-hamburger.svg";
-  }
+  navLinks.classList.toggle("nav-link-active" , isHamburgerIcon);
+ currentNavIcon.src = isHamburgerIcon ? "assets/shared/icon-close.svg" : "assets/shared/icon-hamburger.svg";
 };
 
 navToggler.addEventListener("click", toggle);
